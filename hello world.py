3000c7add58353340825d575cf39587b6346f36d
staffgha <ghall@roundhayschool.com>
2021-12-07 10:22:22 +0000

    Added main game class and constants file. Working game loop, nothing on screen.

--- hello world.py
+++ hello world.py
@@ -1,9 +1,42 @@
-print("hello world")
-print("goodbye")
+import pygame
+import constants
+
+class Main():
+    def __init__(self):
+        # initialise game
+        pygame.init()
+        self.running = True
+        self.screen = pygame.display.set_mode(constants.SIZE)
+
+    def handle_events(self):
+        # check input
+        events = pygame.event.get()
+        for event in events:
+            # if quit
+            if event.type == pygame.QUIT:
+                running = False
 
 
-#This is a comment
+    def update(self):
+        pass
 
-print("hi again")
+    def draw(self):
+        self.screen.fill(constants.BLUE)
+        pygame.display.flip()
 
-#test again
+    def gameloop(self):
+        # game loop
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.draw()
+        pygame.quit()
+
+if __name__ == "__main__":
+    game = Main()
+    game.gameloop()
+
+
+
+
+
