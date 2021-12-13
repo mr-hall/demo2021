8242694bdac0d1ab4ebabe5cf9aa875e998c42b3
staffgha <ghall@roundhayschool.com>
2021-12-13 14:46:26 +0000

    enemies die when they touch a player

--- screens.py
+++ screens.py
@@ -19,21 +19,20 @@ class Game_screen(Screen):
     def __init__(self):
         super().__init__()
         self.player = sprites.Player()
-        self.enemies = []
+        self.enemies = pygame.sprite.Group()
         for i in range(10):
             enemy = sprites.Enemy()
-            self.enemies.append(enemy)
+            self.enemies.add(enemy)
         self.player.reset()
 
     def draw(self,window):
         window.fill(constants.BLUE)
         self.player.draw(window)
-        for enemy in self.enemies:
-            enemy.draw(window)
+        self.enemies.draw(window)
 
     def update(self):
         self.player.update()
-        for enemy in self.enemies:
-            enemy.update()
+        self.enemies.update()
+        pygame.sprite.spritecollide(self.player,self.enemies,True)
 
 
--- sprites.py
+++ sprites.py
@@ -5,11 +5,10 @@ import random
 class Sprite(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.x = 0
-        self.y = 0
+        self.rect = pygame.Rect(0, 0, 0, 0)
 
     def draw(self, window):
-        window.blit(self.image, (self.x, self.y))
+        window.blit(self.image, self.rect)
 
     def update(self):
         pass
@@ -20,22 +19,25 @@ class Player(Sprite):
         super().__init__()
         self.image = pygame.image.load("images/playerShip1_red.png").convert()
         self.image.set_colorkey(constants.BLACK)
+        self.rect = self.image.get_rect()
 
     def update(self):
-        self.x = self.x + 1
-        if self.x > constants.SIZE[0]:
-            self.x = 0
+        self.rect = self.rect.move(1, 0)
+
+        if self.rect.x > constants.SIZE[0]:
+            self.rect.x = 0
 
     def reset(self):
-        self.x = constants.SIZE[0]//2
-        self.y = constants.SIZE[1]-100
+        self.rect.x = constants.SIZE[0]//2
+        self.rect.y = constants.SIZE[1]-100
 
 class Enemy(Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("images/enemyGreen2.png").convert()
         self.image.set_colorkey(constants.BLACK)
-        self.x = random.randint(0,constants.SIZE[0])
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0,constants.SIZE[0])
     #
     def update(self):
-        self.y=self.y+1
+        self.rect.y=self.rect.y+1
